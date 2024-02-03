2ea5ef9afc358d366b11fc8af6d399aa80202210
R-Owino <sayeakinyi@gmail.com>
2024-02-03 10:03:00 +0300

    refactor: import pages and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import '../assets/styles/App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Features from "./pages/Features/Features";
+import Navbar from './components/ui/Navbar';
+import "./assets/styles/App.css";
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  )
 }
-
+ 
 export default App;
