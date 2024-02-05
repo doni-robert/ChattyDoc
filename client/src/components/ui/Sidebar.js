64aaf7422dbad9ae81784d73a0564d7d13319c0f
doni-robert <irobertndungu@gmail.com>
2024-02-05 12:11:03 +0300

    chore: add icons to menu items

--- client/src/components/ui/Sidebar.js
+++ client/src/components/ui/Sidebar.js
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 
 
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PowerSettingsNewOutlinedIcon from '@mui/icons-material/PowerSettingsNewOutlined';
+
 import "../../assets/styles/sidebar.css";
 
 const Bar = () => {
@@ -13,13 +21,13 @@ const Bar = () => {
             <MenuItem className="menu1">
               <h2>Hi User</h2>
             </MenuItem>
-            <MenuItem> HOME </MenuItem>
-            <MenuItem> CHAT </MenuItem>
-            <MenuItem> CONTACT </MenuItem>
-            <MenuItem> NOTIFICATION </MenuItem>
-            <MenuItem> CALENDAR </MenuItem>
-            <MenuItem> SETTINGS </MenuItem>
-            <MenuItem> LOGOUT </MenuItem>
+            <MenuItem icon={<GridViewOutlinedIcon />}> HOME </MenuItem>
+            <MenuItem icon={<ChatBubbleOutlineOutlinedIcon />}> CHAT </MenuItem>
+            <MenuItem icon={<PersonOutlinedIcon />}> CONTACT </MenuItem>
+            <MenuItem icon={<NotificationsNoneOutlinedIcon />}> NOTIFICATION </MenuItem>
+            <MenuItem icon={<CalendarMonthOutlinedIcon />}> CALENDAR </MenuItem>
+            <MenuItem icon={<SettingsOutlinedIcon />}> SETTINGS </MenuItem>
+            <MenuItem icon={<PowerSettingsNewOutlinedIcon />}> LOGOUT </MenuItem>
           </Menu>
         </Sidebar>
         <h1>More Content Coming</h1>
