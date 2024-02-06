d67e15c71c01c09647ce7fc93d27b121a4b3c764
doni-robert <irobertndungu@gmail.com>
2024-02-06 18:42:35 +0300

    update: clean up and comment

--- client/src/components/ui/Sidebar.js
+++ client/src/components/ui/Sidebar.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-import { useState } from 'react';
-
-
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
@@ -11,24 +7,16 @@ import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNone
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import PowerSettingsNewOutlinedIcon from '@mui/icons-material/PowerSettingsNewOutlined';
-import WelcomeMessage from './WelcomeMessage';
-
-import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
-import ChatTest from './ChatTest';
 
 
-
-
-const Bar = ({ setSelectedComponent }) => {
+const SideBar = ({ setSelectedComponent }) => {
 
     return (
       <div>
        <Sidebar style={{ display: "flex", height: "100vh" }} className="app">
           <Menu>
-            <MenuItem className="menu1">
-              <h2>Hi User</h2>
-            </MenuItem>
-            <MenuItem onClick={() => setSelectedComponent('welcomeMessage')} icon={<ChatBubbleOutlineOutlinedIcon />}>  HOME  </MenuItem>
+            <MenuItem className="menu1"><h2> Hi User </h2></MenuItem>
+            <MenuItem onClick={() => setSelectedComponent('welcomeMessage')} icon={<GridViewOutlinedIcon />}>  HOME  </MenuItem>
             <MenuItem onClick={() => setSelectedComponent('chat')} icon={<ChatBubbleOutlineOutlinedIcon />} > CHAT </MenuItem>
             <MenuItem onClick={() => setSelectedComponent('contacts')} icon={<PersonOutlinedIcon />}> CONTACT </MenuItem>
             <MenuItem onClick={() => setSelectedComponent('notifications')} icon={<NotificationsNoneOutlinedIcon />}> NOTIFICATION </MenuItem>
@@ -37,20 +25,7 @@ const Bar = ({ setSelectedComponent }) => {
             <MenuItem icon={<PowerSettingsNewOutlinedIcon />}> LOGOUT </MenuItem>
           </Menu>
         </Sidebar>
-        <div>
-          <Routes >
-            
-            <Route path="chattest" element={<ChatTest />} />
-
-          </Routes>
-          
-        </div>
-      
-
-      </div>
-      
- 
-      
+      </div> 
     );
   };
-  export default Bar;
+  export default SideBar;
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,21 +1,24 @@
 import React from 'react'
-import Bar from '../../components/ui/Sidebar'
+import SideBar from '../../components/ui/Sidebar'
 import WelcomeMessage from '../../components/ui/WelcomeMessage'
-import { Outlet } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import ChatTest from '../../components/ui/ChatTest';
 import { useState } from 'react';
 import Notifications from '../../components/ui/Notifications'
 import Contacts from '../../components/ui/Contacts'
 
 
-
 const Dashboard = () => {
+  //State for selecting component to display beside the SideBar
+  // Default set to 'dashboardHome'
+  const [selectedComponent, setSelectedComponent] = useState('welcomeMessage');
 
-  const [selectedComponent, setSelectedComponent] = useState('chat');
   return (
     <div style={{ display: 'flex' }}>
-      <Bar setSelectedComponent={setSelectedComponent}/>
+
+      <SideBar setSelectedComponent={setSelectedComponent}/>
+
+      {/* Conditional rendering to render the selectedComponent.
+          Currently renders dummy components */}
       {selectedComponent === 'chat' && <ChatTest />}
       {selectedComponent === 'contacts' && <Contacts />}
       {selectedComponent === 'notifications' && <Notifications />}
