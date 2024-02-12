0b9f674f87ac3800ee5926daa4e88bb511073a2a
doni-robert <irobertndungu@gmail.com>
2024-02-12 23:14:44 +0300

    update: improve dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import SideBar from '../../components/ui/Sidebar'
-import HomeDashboard from '../../components/ui/HomeDashboard'
 import Chat from '../../components/ui/Chat';
 import { useState } from 'react';
 import Notifications from '../../components/ui/Notifications'
 import Contacts from '../../components/ui/Contacts'
 import "../../assets/styles/dashboard.css";
+import HomeDashboard from './HomeDashboard';
 
 
 const Dashboard = () => {
@@ -16,8 +16,7 @@ const Dashboard = () => {
   return (
     <div className='dashboard-container'>
 
-      <SideBar setSelectedComponent={setSelectedComponent}
-              />
+      <SideBar setSelectedComponent={setSelectedComponent}/>
 
       {/* Conditional rendering to render the selectedComponent.
           Currently renders dummy components */}
