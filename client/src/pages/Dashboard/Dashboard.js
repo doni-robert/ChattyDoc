0e617ffd6deccf08f7493e74ffbba05f6f38056b
R-Owino <sayeakinyi@gmail.com>
2024-02-19 14:28:28 +0300

    chore: import chat list to dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import SideBar from '../../components/ui/Sidebar'
-import Chat from '../../components/ui/Chat';
 import { useState } from 'react';
 import Notifications from '../../components/ui/Notifications'
 import Contacts from '../../components/ui/Contacts'
 import "../../assets/styles/dashboard.css";
 import HomeDashboard from './HomeDashboard';
+import Chats from './Chats'
 
 
 const Dashboard = () => {
   //State for selecting component to display beside the SideBar
   // Default set to 'dashboardHome'
   const [selectedComponent, setSelectedComponent] = useState('homeDashboard');
+  
 
   return (
     <div className='dashboard-container'>
@@ -20,7 +21,7 @@ const Dashboard = () => {
 
       {/* Conditional rendering to render the selectedComponent.
           Currently renders dummy components */}
-      {selectedComponent === 'chat' && <Chat />}
+      {selectedComponent === 'chat' && <Chats />}
       {selectedComponent === 'contacts' && <Contacts />}
       {selectedComponent === 'notifications' && <Notifications />}
       {selectedComponent === 'homeDashboard' && <HomeDashboard />}  
