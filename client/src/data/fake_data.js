4f152d1eaf05d0523d9d446d10a1bf3357b69dbd
R-Owino <sayeakinyi@gmail.com>
2024-02-23 21:22:48 +0300

    chore: added fake data for the chat area

--- client/src/data/fake_data.js
+++ client/src/data/fake_data.js
@@ -83,4 +83,75 @@ const ChatList = [
   },
 ];
 
-export { ChatList };
+const Chat_History = [
+  {
+    type: "msg",
+    message: "Hi 👋🏻, How are ya ?",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "divider",
+    text: "Today",
+  },
+  {
+    type: "msg",
+    message: "Hi 👋 Rob, not bad, u ?",
+    incoming: false,
+    outgoing: true,
+  },
+  {
+    type: "msg",
+    message: "Can you send me an abstarct image?",
+    incoming: false,
+    outgoing: true,
+  },
+  {
+    type: "msg",
+    message: "Ya sure, sending you a pic",
+    incoming: true,
+    outgoing: false,
+  },
+
+  {
+    type: "msg",
+    subtype: "img",
+    message: "Here You Go",
+    img: faker.image.urlLoremFlickr({ category: 'abstract' }),
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    message: "Can you please send this in file format?",
+    incoming: false,
+    outgoing: true,
+  },
+
+  {
+    type: "msg",
+    subtype: "doc",
+    message: "Yes sure, here you go.",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "link",
+    preview: faker.image.urlLoremFlickr({ category: 'animals' }),
+    message: "Yep, I can also do that",
+    incoming: true,
+    outgoing: false,
+  },
+  {
+    type: "msg",
+    subtype: "reply",
+    reply: "This is a reply",
+    message: "Yep, I can also do that",
+    incoming: false,
+    outgoing: true,
+  },
+];
+
+
+export { ChatList, Chat_History };
