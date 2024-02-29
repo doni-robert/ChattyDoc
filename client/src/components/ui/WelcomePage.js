33973124a17f6c5c27e979e6dba89b86b9c57c12
R-Owino <sayeakinyi@gmail.com>
2024-02-29 16:33:02 +0300

    update: used Material UI for styling snce creating a css file conflicted with the styling for homeDashboard

--- client/src/components/ui/WelcomePage.js
+++ client/src/components/ui/WelcomePage.js
@@ -1,26 +1,33 @@
-import {React, useState} from "react";
-import "../../assets/styles/welcomePage.css";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Stack, Typography, Container } from "@mui/material";
+import welcomeImage from "../../assets/images/welcome.jpg";
 
 const WelcomePage = () => {
-  const [page, setPage] = useState('chats');
-
-  const getMessage = () => {
-    if (page === 'chats') {
-      return 'Select a chat to start messaging';
-    } else if (page === 'contacts') {
-      return 'Select a contact to start messaging';
-    }
-  };
-
   return (
-    <div className="welcome-page">
-      <div className="background-image">
-        <img src="../../assets/images/welcome.jpg" alt="Doctor chatting with a patient" />
-      </div>
-      <div className="welcome-message">
-        <p>{getMessage()}</p>
-      </div>
-    </div>
+    <>
+      <Container sx={{ mt: 10 }} maxWidth="sm">
+        <Stack spacing={5}>
+          <Stack
+            sx={{ width: "100%" }}
+            direction="column"
+            alignItems={"center"}
+          >
+            <img
+              style={{ height: 300, width: 500, marginTop: "50px" }}
+              src={welcomeImage}
+              alt="welcome"
+            />
+          </Stack>
+          <Outlet />
+          <Stack alignItems={"center"}>
+            <Typography variant="h5">
+              Select a chat to start messaging
+            </Typography>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
   );
 };
 
