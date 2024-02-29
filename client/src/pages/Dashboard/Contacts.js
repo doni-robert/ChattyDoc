036f92ac080081ce84529e56320ab6e37409de1f
R-Owino <sayeakinyi@gmail.com>
2024-02-29 16:37:31 +0300

    update: add the welcome page section

--- client/src/pages/Dashboard/Contacts.js
+++ client/src/pages/Dashboard/Contacts.js
@@ -4,8 +4,8 @@ import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import { Divider, IconButton, Avatar } from "@mui/material";
 import { Contact_List } from "../../data/fake_data";
 import { faker } from "@faker-js/faker";
+import WelcomePage from "../../components/ui/WelcomePage";
 import "../../assets/styles/contacts.css";
-
 
 const ContactListElement = ({ id, img, name, about }) => {
   return (
@@ -27,39 +27,54 @@ const ContactListElement = ({ id, img, name, about }) => {
 
 const Contacts = () => {
   return (
-    <div className="contacts-container">
-      {/* create new group */}
-      <div className="new-group">
-        <div className="icon">
-          <IconButton>
-            <GroupIcon />
-          </IconButton>
-        </div>
-        <div className="create-group">
-          <p>Create New Group</p>
-        </div>
-      </div>
+    <div
+      style={{
+        backgroundColor: "#fff",
+        height: "100vh",
+        width: "calc(100vw - 250px)",
+      }}
+    >
+      <div className="contacts-section">
+        <div className="contacts-container">
+          {/* create new group */}
+          <div className="new-group">
+            <div className="icon">
+              <IconButton>
+                <GroupIcon />
+              </IconButton>
+            </div>
+            <div className="create-group">
+              <p>Create New Group</p>
+            </div>
+          </div>
 
-      {/* create new contact */}
-      <div className="new-contact">
-        <div className="icon">
-          <IconButton>
-            <PersonAddIcon />
-          </IconButton>
-        </div>
-        <div className="add-contact">
-          <p>Add New Contact</p>
-        </div>
-      </div>
+          {/* create new contact */}
+          <div className="new-contact">
+            <div className="icon">
+              <IconButton>
+                <PersonAddIcon />
+              </IconButton>
+            </div>
+            <div className="add-contact">
+              <p>Add New Contact</p>
+            </div>
+          </div>
 
-      <Divider />
+          <Divider />
 
-      {/* Contact list */}
-      <div className="contact-list">
-        <p>Available Contacts</p>
-        {Contact_List.map((el) => {
-          return <ContactListElement {...el} />;
-        })}
+          {/* Contact list */}
+          <div className="contact-list">
+            <p>Available Contacts</p>
+            {Contact_List.map((el) => {
+              return <ContactListElement {...el} />;
+            })}
+          </div>
+        </div>
+
+        {/* welcome page section */}
+        <div className="welcome">
+          <WelcomePage />
+        </div>
       </div>
     </div>
   );
