f324d5f5a5ee8d539056c8ec9cd2e209b54659e2
R-Owino <sayeakinyi@gmail.com>
2024-03-03 13:01:42 +0300

    feature: user profile

---
+++ client/src/components/ui/Profile.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../assets/styles/profile.css";
+import UserInfo from "../forms/UserInfo";
+import UpdateUserInfo from "../forms/UpdateUserInfo";
+
+const Profile = ({ userInfo, setUserInfo, userImage, setUserImage }) => {
+  return (
+    <div className="profile-container">
+      <div className="user-info-container">
+        <UserInfo userInfo={userInfo} userImage={userImage} />
+      </div>
+      <div className="update-info-container">
+        <UpdateUserInfo
+          userInfo={userInfo}
+          setUserInfo={setUserInfo}
+          userImage={userImage}
+          setUserImage={setUserImage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
