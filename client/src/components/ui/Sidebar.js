07ae57bb9ca774cebf4c1b1908f7f61309690b20
R-Owino <sayeakinyi@gmail.com>
2024-03-03 12:43:34 +0300

    feature: added profile and groups components

--- client/src/components/ui/Sidebar.js
+++ client/src/components/ui/Sidebar.js
@@ -1,70 +1,76 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import PowerSettingsNewOutlinedIcon from '@mui/icons-material/PowerSettingsNewOutlined';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import PowerSettingsNewOutlinedIcon from "@mui/icons-material/PowerSettingsNewOutlined";
 import "../../assets/styles/sidebar.css";
 
+const SideBar = ({ setSelectedComponent, userImage }) => {
+  // State for tracing the selected menu
+  const [selectedMenuItem, setSelectedMenuItem] = useState("");
 
-const SideBar = ({ setSelectedComponent }) => {
-    // State for tracing the selected menu
-    const [selectedMenuItem, setSelectedMenuItem] = useState('');
-
-    const handleMenuItemClick = (component) => {
-      setSelectedMenuItem(component);
-      setSelectedComponent(component);
-    };
-
-    return (
-      <div className="sidebar-container">
-       <Sidebar className="sidebar">
-          <Menu>
-              <MenuItem className="user-profile">
-                <h2> Hi User </h2>
-              </MenuItem>
-              <MenuItem 
-                onClick={() => handleMenuItemClick('homeDashboard')} 
-                icon={<HomeOutlinedIcon />}
-                className={selectedMenuItem === 'homeDashboard' ? 'selected' : ''}
-              >
-                Home 
-              </MenuItem>
-              <MenuItem 
-                onClick={() => handleMenuItemClick('chat')} 
-                icon={<ChatBubbleOutlineOutlinedIcon />}
-                className={selectedMenuItem === 'chat' ? 'selected' : ''}
-              > 
-                Chats
-              </MenuItem>
-              <MenuItem 
-                onClick={() => handleMenuItemClick('contacts')} 
-                icon={<PersonOutlinedIcon />}
-                className={selectedMenuItem === 'contacts' ? 'selected' : ''}
-              >
-                Contacts
-              </MenuItem>
-              <MenuItem 
-                onClick={() => handleMenuItemClick('notifications')}
-                icon={<NotificationsNoneOutlinedIcon />}
-                className={selectedMenuItem === 'notification' ? 'selected' : ''}
-              >
-                Notifications
-              </MenuItem>
-              <MenuItem icon={<CalendarMonthOutlinedIcon />}> Calendar </MenuItem>
-              <MenuItem icon={<SettingsOutlinedIcon />}> Settings </MenuItem>
-              {/* Lazy coding. Couldn't come up with a better way to push LOGOUT to the bottom */}
-             
-              <MenuItem></MenuItem>
-              <MenuItem></MenuItem>
-              <MenuItem></MenuItem>
-              <MenuItem icon={<PowerSettingsNewOutlinedIcon />}> Logout </MenuItem>
-            </Menu>
-        </Sidebar>
-      </div> 
-    );
+  const handleMenuItemClick = (component) => {
+    setSelectedMenuItem(component);
+    setSelectedComponent(component);
   };
-  export default SideBar;
+
+  const handleLogout = () => {
+    // clear token from local storage
+    localStorage.removeItem("jwtToken");
+
+    // reload page to navigate to login
+    window.location.reload();
+  };
+
+  return (
+    <div className="sidebar-container">
+      <Sidebar className="sidebar">
+        <Menu>
+          <div
+            onClick={() => handleMenuItemClick("profile")}
+            className="user-profile"
+          >
+            <img src={userImage} alt="profile pic" className="profile-pic" />
+          </div>
+          <MenuItem
+            onClick={() => handleMenuItemClick("homeDashboard")}
+            icon={<HomeOutlinedIcon />}
+            className={selectedMenuItem === "homeDashboard" ? "selected" : ""}
+          >
+            Home
+          </MenuItem>
+          <MenuItem
+            onClick={() => handleMenuItemClick("chat")}
+            icon={<ChatBubbleOutlineOutlinedIcon />}
+            className={selectedMenuItem === "chat" ? "selected" : ""}
+          >
+            Chats
+          </MenuItem>
+          <MenuItem
+            onClick={() => handleMenuItemClick("groups")}
+            icon={<GroupOutlinedIcon />}
+            className={selectedMenuItem === "groups" ? "selected" : ""}
+          >
+            Groups
+          </MenuItem>
+          <MenuItem
+            onClick={() => handleMenuItemClick("contacts")}
+            icon={<PersonOutlinedIcon />}
+            className={selectedMenuItem === "contacts" ? "selected" : ""}
+          >
+            Contacts
+          </MenuItem>
+          <MenuItem
+            icon={<PowerSettingsNewOutlinedIcon />}
+            onClick={handleLogout}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+    </div>
+  );
+};
+export default SideBar;
