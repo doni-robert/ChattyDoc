bd8bd262025457f21d5ec23bb280ee0203d849ec
R-Owino <sayeakinyi@gmail.com>
2024-03-08 08:13:13 +0300

    feat: socket connection

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import io from "socket.io-client";
+const socket = io("http://localhost:5000");
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App socket={socket}/>
   </React.StrictMode>
 );
